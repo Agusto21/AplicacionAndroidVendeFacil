d19d453d9ce3eb18496d4cd03c694d0773c6c4ce
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 21:48:54 -0500

    Update index.mjs

--- lambdaAWS_BD/index.mjs
+++ lambdaAWS_BD/index.mjs
@@ -7,14 +7,14 @@ export const handler = async (event) => {
     const method = event.requestContext.http.method;
     const path = event.rawPath;
     
-    // Obtenemos el usuario_id de los headers (tu token o identificador)
+
     const usuarioId = event.headers?.['x-usuario-id']; 
 
     if (path === "/login" && method === "POST") {
       return usuarioController.login(event);
     }
 
-    // --- RUTAS PARA USUARIOS ---
+
     if (path.startsWith("/usuarios")) {
       const parts = path.split('/');
       const id = parts.length > 2 ? parts[2] : null;
@@ -25,12 +25,12 @@ export const handler = async (event) => {
       if (method === "DELETE" && id) return usuarioController.deleteUsuario(event);
     }
 
-    // Validación global de multi-tenancy para el resto de rutas
+
     if (!usuarioId) {
       return { statusCode: 401, body: JSON.stringify({ message: "Falta x-usuario-id en headers" }) };
     }
 
-    // --- RUTAS PARA PRODUCTOS ---
+
     if (path.startsWith("/productos")) {
       const parts = path.split('/');
       const id = parts.length > 2 ? parts[2] : null;
@@ -42,7 +42,7 @@ export const handler = async (event) => {
       if (method === "DELETE" && id) return productoController.deleteProducto(usuarioId, id);
     }
 
-    // --- RUTAS PARA VENTAS ---
+
     if (path.startsWith("/ventas")) {
       const parts = path.split('/');
       const id = parts.length > 2 ? parts[2] : null;
