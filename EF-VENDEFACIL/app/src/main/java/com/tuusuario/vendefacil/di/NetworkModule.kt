d4412b5d76ecd0de8a3c9e06805f7585a4076b92
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 20:20:51 -0500

    Update NetworkModule.kt

--- EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/di/NetworkModule.kt
+++ EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/di/NetworkModule.kt
@@ -5,15 +5,15 @@ import com.tuusuario.vendefacil.core.network.ApiService
 import com.tuusuario.vendefacil.core.network.RetrofitClient
 import com.tuusuario.vendefacil.core.utils.SessionManager
 
-// Añadimos el Context al constructor principal
+
 class NetworkModule(private val context: Context) {
 
-    // Instanciamos el SessionManager
+
     val sessionManager: SessionManager by lazy {
         SessionManager(context)
     }
 
-    // Le pasamos el SessionManager al RetrofitClient
+
     val apiService: ApiService by lazy {
         RetrofitClient.createRetrofit(sessionManager).create(ApiService::class.java)
     }
