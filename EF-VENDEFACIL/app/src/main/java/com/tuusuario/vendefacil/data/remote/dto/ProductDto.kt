0e8ce46fe27894141b1e8f05831ae18548e03fb0
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 20:17:48 -0500

    Update ProductDto.kt

--- EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/remote/dto/ProductDto.kt
+++ EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/remote/dto/ProductDto.kt
@@ -3,7 +3,7 @@ package com.tuusuario.vendefacil.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class ProductDto(
-    @SerializedName("producto_id") val productoId: String? = null, // AWS lo genera al crear
+    @SerializedName("producto_id") val productoId: String? = null, 
     @SerializedName("nombre") val nombre: String,
     @SerializedName("categoria") val categoria: String? = null,
     @SerializedName("precio") val precio: Double,
