b56b0f64992fdd65b5670a5bbd45c8bca751ac98
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 20:16:34 -0500

    Update ProductMapper.kt

--- EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/mapper/ProductMapper.kt
+++ EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/mapper/ProductMapper.kt
@@ -3,7 +3,7 @@ package com.tuusuario.vendefacil.data.mapper
 import com.tuusuario.vendefacil.data.remote.dto.ProductDto
 import com.tuusuario.vendefacil.domain.model.Product
 
-// De AWS hacia la App (Lectura)
+
 fun ProductDto.toDomain() = Product(
     id = this.productoId ?: "",
     name = this.nombre,
@@ -12,7 +12,7 @@ fun ProductDto.toDomain() = Product(
     stock = this.stock
 )
 
-// De la App hacia AWS (Escritura)
+
 fun Product.toDto() = ProductDto(
     productoId = if (this.id.isEmpty()) null else this.id, // Si está vacío, enviamos null para que AWS lo cree
     nombre = this.name,
