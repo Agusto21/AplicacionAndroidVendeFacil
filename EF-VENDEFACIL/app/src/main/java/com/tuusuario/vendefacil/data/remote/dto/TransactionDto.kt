89bd936f073b8cc79ab1d5641035e7c92461d840
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 20:18:06 -0500

    Update TransactionDto.kt

--- EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/remote/dto/TransactionDto.kt
+++ EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/remote/dto/TransactionDto.kt
@@ -3,8 +3,8 @@ package com.tuusuario.vendefacil.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class TransactionDto(
-    @SerializedName("venta_id") val ventaId: String? = null, // AWS lo genera
-    @SerializedName("fecha") val fecha: String? = null,      // AWS lo genera
+    @SerializedName("venta_id") val ventaId: String? = null, 
+    @SerializedName("fecha") val fecha: String? = null,     
     @SerializedName("cliente") val cliente: String,
     @SerializedName("metodopago") val metodoPago: String,
     @SerializedName("total") val total: Double,
