0a3a4571a95ddb9cdadf228238f120603fd72c3e
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 20:17:27 -0500

    Update ApiResponse.kt

--- EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/remote/dto/ApiResponse.kt
+++ EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/remote/dto/ApiResponse.kt
@@ -2,8 +2,6 @@ package com.tuusuario.vendefacil.data.remote.dto
 
 import com.google.gson.annotations.SerializedName
 
-// La 'T' es un tipo genérico. Significa que "data" puede ser 
-// cualquier cosa: una lista de productos, los datos de un usuario, etc.
 data class ApiResponse<T>(
     @SerializedName("success")
     val success: Boolean,
