0747482fe31e6094866bdd8c42c04fc12505f546
TowersOne <134395792+Agusto21@users.noreply.github.com>
2026-07-07 20:19:38 -0500

    Update SalesRepositoryImpl.kt

--- EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/repository/SalesRepositoryImpl.kt
+++ EF-VENDEFACIL/app/src/main/java/com/tuusuario/vendefacil/data/repository/SalesRepositoryImpl.kt
@@ -19,12 +19,12 @@ class SalesRepositoryImpl(
     private val _transactions = MutableStateFlow<List<Transaction>>(emptyList())
     override fun getTransactions(): Flow<List<Transaction>> = _transactions
 
-    // NUEVO: Método para descargar el historial desde AWS
+
     override suspend fun fetchTransactions() {
         try {
             val response = remoteDataSource.getVentas()
             if (response.isSuccessful && response.body()?.success == true) {
-                // Obtenemos la lista de DTOs y mapeamos cada uno a Dominio
+
                 val dtoList = response.body()!!.data
                 _transactions.value = dtoList.map { it.toDomain() }
             } else {
@@ -39,7 +39,7 @@ class SalesRepositoryImpl(
         try {
             val response = remoteDataSource.createVenta(transaction.toDto())
             if (response.isSuccessful && response.body()?.success == true) {
-                // Volvemos a descargar todo de AWS para asegurar consistencia
+
                 fetchTransactions()
             } else {
                 Log.e("API_VENTAS", "Error creando venta: ${response.errorBody()?.string()}")
@@ -68,7 +68,7 @@ class SalesRepositoryImpl(
         try {
             val response = remoteDataSource.deleteVenta(transactionId)
             if (response.isSuccessful) {
-                fetchTransactions() // Recargamos la lista
+                fetchTransactions() 
             }
         } catch (e: Exception) {
             e.printStackTrace()
